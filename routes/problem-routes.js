6691d56a1190c0c3089f82e023a23e47b2c441a4
Greg Glinoga <gglinoga@gmail.com>
2019-04-30 13:41:37 -0700

    submit is done

--- routes/problem-routes.js
+++ routes/problem-routes.js
@@ -36,6 +36,7 @@ module.exports = (app) => {
 
     app.post("/api/problems", (req, res) => {
         model.Problem.create(req.body).then((dbProblem) => {
+            console.log('post problems')
             console.log(dbProblem);
             res.json(dbProblem);
         });
